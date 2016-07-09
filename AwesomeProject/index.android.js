0df7674fd035210ebe2716dfbcbf7bfb5fd05c97
Nazmul Idris <nazmul@fasterlap.com>
2016-07-09 12:56:13 -0700

    adding image loading and state to the app

--- AwesomeProject/index.android.js
+++ AwesomeProject/index.android.js
@@ -6,11 +6,7 @@
 
 import React, {Component} from 'react';
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    ToastAndroid
+    AppRegistry, StyleSheet, Text, View, ToastAndroid, Image
 } from 'react-native';
 
 /**
@@ -21,22 +17,35 @@ import Button from 'react-native-button';
 
 class AwesomeProject extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            img_url: "https://goo.gl/vRNu5m"
+        }
+        // this.update = this.update.bind(this);
+    }
+
+    update(url) {
+        this.setState({img_url: url});
+    }
+
     render() {
+        let dateFormat = require('dateformat')
+        let formattedDate = dateFormat(Date.now(), "dddd, mmmm dS, yyyy, h:MM:ss TT");
+
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
-                    Welcome to React Native!
+                    {formattedDate}
                 </Text>
                 <Text style={styles.instructions}>
-                    To get started, edit index.android.js
+                    To get started, edit index.android.js. Shake or press menu button for dev menu
                 </Text>
-                <Text style={styles.instructions}>
-                    Shake or press menu button for dev menu
-                </Text>
+                <Image source={{uri: this.state.img_url}} style={styles.image}/>
                 <Button
                     containerStyle={styles.buttonContainer}
                     style={styles.button}
-                    onPress={this.buttonPressed}>
+                    onPress={this.buttonPressed.bind(this)}>
                     Press Me!
                 </Button>
             </View>
@@ -44,11 +53,17 @@ class AwesomeProject extends Component {
     }
 
     buttonPressed() {
+        this.update("https://goo.gl/B94hp8");
         ToastAndroid.show("YAY! Button is pressed!", ToastAndroid.LONG);
     }
 }
 
 const styles = StyleSheet.create({
+    image: {
+        margin: 20,
+        width: 150,
+        height: 150,
+    },
     buttonContainer: {
         padding: 10,
         borderColor: '#CACACA',
