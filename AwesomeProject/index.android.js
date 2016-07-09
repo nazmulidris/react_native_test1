5fef775e98e312d4a0413fad1131bb94871fb43c
Nazmul Idris <nazmul@fasterlap.com>
2016-07-08 22:24:55 -0700

    initial commit of react native project w/ react native button added

---
+++ AwesomeProject/index.android.js
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    ToastAndroid
+} from 'react-native';
+
+var Button = require('react-native-button');
+
+class AwesomeProject extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.android.js
+                </Text>
+                <Text style={styles.instructions}>
+                    Shake or press menu button for dev menu
+                </Text>
+                <Button
+                    style={{borderWidth: 1, borderColor: 'blue'}}
+                    onPress={this.buttonPressed}>
+                    Press Me!
+                </Button>
+            </View>
+        );
+    }
+
+    buttonPressed() {
+        ToastAndroid.show("Button is pressed", ToastAndroid.LONG);
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
