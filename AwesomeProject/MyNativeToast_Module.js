7e3fd87445c480cfd4f4cafaf00d9e4062335d46
Nazmul Idris <nazmul@fasterlap.com>
2016-07-10 15:53:27 -0700

    1) added docs to js module wrapper

--- AwesomeProject/MyNativeToast_Module.js
+++ AwesomeProject/MyNativeToast_Module.js
@@ -4,6 +4,8 @@
  * function 'show' which takes the following parameters:
  * 1. String message: A string with the text to toast
  * 2. int duration: The duration of the toast. May be MyNativeToast.SHORT or MyNativeToast.LONG
+ *
+ * There are 3 variants of this function ... one is simple, one is w/ callbacks, and one is w/ promise!
  */
 import {NativeModules} from 'react-native';
 module.exports = NativeModules.MyNativeToast;
